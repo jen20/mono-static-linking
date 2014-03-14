0e897acdb0615726bda5f4a9407e954b5f9b5247
James Nugent <james.nugent@bathasu.com>
2014-03-14 14:13:59 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mcs -target:exe -out:bin/helloworld.exe src/Program.cs
---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StaticLinkingWtf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
